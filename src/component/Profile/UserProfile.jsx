08078aa6868cf20c97bfb30e8a018c9aaeb6da28
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2024-10-29 20:39:35 +0530

    Update UserProfile.jsx

--- src/component/Profile/UserProfile.jsx
+++ src/component/Profile/UserProfile.jsx
@@ -3,15 +3,17 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Button } from '@mui/material';
 
 const UserProfile = () => {
+    const email = useSelector((state) => state.auth.user?.email || "");
+    const fullName = useSelector((state) => state.auth.user?.fullName || "");
     const handleLogout = () => {
 
     }
     return (
         <div className='min-h-[80vh] flex flex-col items-center justify-center text-center'>
-            <div className='flex flex-col items-center justify-center'>
+            <div className='fixed flex flex-col items-center justify-center'>
                 <AccountCircleIcon sx={{ fontSize: "9rem" }} />
-                <h1 className='py-5 text-2xl font-semibold'>Trinadh</h1>
-                <p>Email: triandhsai@gmail.com</p>
+                <h1 className='py-5 text-2xl font-semibold'>{fullName}</h1>
+                <p>Email: {email}</p>
                 <Button variant='contained' onClick={handleLogout} sx={{ margin: "2rem 0rem" }}>Logout</Button>
             </div>
         </div>
